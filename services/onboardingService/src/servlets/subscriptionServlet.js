a2c248c69e223bff48ebd9c7ad3d072da7c91b4c
theGreatHeisenberg <shreyas.panhalkar@221b-Baker-street.(none)>
2015-11-28 18:06:59 +0530

    adding upsert function

--- services/onboardingService/src/servlets/subscriptionServlet.js
+++ services/onboardingService/src/servlets/subscriptionServlet.js
@@ -32,10 +32,31 @@ var subscription = function(logger, configuration) {
                         if (err == null && !isEmpty(returnObject)) {
                             logger.info("asd" + returnObject);
                             logger.info("merchant account already exists. updating paymentOptions: " + body.paymentOptions);
-                            var paymentOptions = {"paymentOptions" : body.paymentOptions};
+
+                            var existingPaymentOptions =
+                                returnObject[0].paymentOptions;
+                            logger.info("existing payment options: " + existingPaymentOptions);
+                            var done = [];
+                            for (var i = 0 ; i < body.paymentOptions.length ; i++){
+                                for (var j = 0 ; j < existingPaymentOptions.length; j++){
+                                    if (body.paymentOptions[i].name == existingPaymentOptions[j].name) {
+                                        existingPaymentOptions[j].isActive = body.paymentOptions[i].isActive
+                                        done.push(body.paymentOptions[i].name);
+                                    }
+                                    else {
+                                        break;
+                                    }
+                                }
+                            }
+                            for (var i = 0 ; i < body.paymentOptions.length ; i++) {
+                                if (done.indexOf(body.paymentOptions[i].name) == -1) {
+                                    existingPaymentOptions.push(body.paymentOptions[i])
+                                }
+                            }
+                            var paymentOptions = {"paymentOptions" : existingPaymentOptions};
                             configuration.updateToDb(
                                 selectionobject,
-                                body.paymentOptions,
+                                paymentOptions,
                                 function(err, updateResponse) {
                                     logger.log("err: " + err);
                                     logger.log("updateResponse: " + updateResponse);
